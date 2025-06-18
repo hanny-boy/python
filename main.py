b9b8de4edb5c5e3bc8d4aba7363bf1f7f2c4ffec
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-06-18 17:15:56 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-units = int(input("Enter the number of units consumed:20"))
-if units <= 50 :
-    bill = units * 2.60
-elif units <= 100 :
-    bill = (50 * 2.60) + ((units - 50)) * 3.25
-elif units <= 250 :
-    bill = (50 * 2.60) + (50 * 3.25) + ((units - 100) * 4.00)
-elif units <= 500 :
-    bill = (50 * 2.60) + (50 * 3.25) + (150 * 4.00) + ((units - 250) * 4.75)
-else:
-    bill = (50 * 2.60) + (50 * 3.25) + (150 * 4.00) + (250 * 4.75) + ((units - 500) * 5.20)
-print(f"Your total bill is: {bill:.2f} Rs")
+st = input("Enter the string:")
+st2 = ""
+for i in st:
+    st2 = i + st2
+print (f"Reversed string: {st2}")
