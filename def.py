f03442144629bb6ce017170aa44ce3e8a7ba5525
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-07-09 16:46:51 +1200

    Add files via upload

---
+++ def.py
@@ -0,0 +1,3 @@
+def wellwishes():
+    print("Wishing you a day full of joy and success!")
+wellwishes()
---
+++ weather.py
@@ -0,0 +1,6 @@
+def weathercondition():
+    print("The weather is plesant in",Autumn)
+    print("The weather same in ",Summer)
+Autumn = "Summer"
+Summer = "Autumn"
+weathercondition()
