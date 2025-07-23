bd7374e9666dc3794d6bb9c31d21ec9f76665f49
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-07-23 17:13:15 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import math
-
-n = float(input("Enter any number: "))
-
-sqrt_val = math.sqrt(n)
-
-ceil_val = math.ceil(n)
-
-print("Square root of", n, "is", sqrt_val)
-print("Ceiling value of", n, "is", ceil_val)
+def hotelcost(nights):
+    return 140*nights
+def planeridecost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+    else:
+        return 0
+def rentalcarcost(days):
+    cost = 40*days
+def tripcosts(city, days, nights):
+    return hotelcost(nights) + planeridecosr(city) + rentalcarcost(days)
+print("Total currentall costs:", rentalcarcost(5))
+print("Total trip costs:", tripcosts("Los Angeles", 5, 2))
