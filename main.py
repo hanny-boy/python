35e67ab0ec90c97adbfd9aab41e0c7ea634b04ef
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-07-23 16:55:01 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-try:
-    age = int(input("Enter your age: "))
-    print("The age entered is", age)
-except ValueError as e:
-    print("Exception occurred:", e)
-    print("Please enter a valid integer for age.")
+import math
+
+n = float(input("Enter any number: "))
+
+sqrt_val = math.sqrt(n)
+
+ceil_val = math.ceil(n)
+
+print("Square root of", n, "is", sqrt_val)
+print("Ceiling value of", n, "is", ceil_val)
