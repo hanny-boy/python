37b0766ff381d3d57915138bfe22951849cb727b
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-07-30 17:03:04 +1200

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,9 @@
+tuplex = (False,"Hanniel ", 3, 4.5, 300000000)
+print(tuplex)
+tuplex = tuplex + (10000, 10000)
+print(tuplex)
+tuple1 = (1, 9 ,5 ,1 ,2,3,4,2,6 , 3, 7, 8, 9, 10)
+print(tuple1.count(1))
+print(len(tuple1))
+s = tuple1[3:5]
+print(s)
