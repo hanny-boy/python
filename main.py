9b31419823ec7fa8a0540845798cc300b9c7d3a5
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-08-05 16:43:16 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-def hotelcost(nights):
-    return 140*nights
-def planeridecost(city):
-    if city == "Charlotte":
-        return 183
-    elif city == "Tampa":
-        return 220
-    elif city == "Pittsburgh":
-        return 222
-    elif city == "Los Angeles":
-        return 475
-    else:
-        return 0
-def rentalcarcost(days):
-    cost = 40*days
-def tripcosts(city, days, nights):
-    return hotelcost(nights) + planeridecosr(city) + rentalcarcost(days)
-print("Total currentall costs:", rentalcarcost(5))
-print("Total trip costs:", tripcosts("Los Angeles", 5, 2))
+p1 = ( 2, 3, 4, 5, 6 )
+p2 = ( 1, 2, 3, 4, 5 )
+for i in range(1, 6):   
+    r = p1[i] + p2[i]
+    print(i)
