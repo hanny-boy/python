74f807cd0a9d2f8abc6f5ee9ce8b736126d42197
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-08-13 16:42:36 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-student = {'id1': {"name": "Hanniel", 'age': 20, 'grade': 'A'},
-           'id2': {'name': "John", 'age': 22, 'grade': 'B'},
-           'id3': {'name': 'Alice', 'age': 21, 'grade': 'A'}}
-result = {}
-for i, j in student.items():
-    if j not in result.values():
-        result[i] = j
-print(result)
+l = ["apple","banana","cherry", "blueberry", "mango"]
+for i in l:
+    print(i.title())
