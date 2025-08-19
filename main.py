2e507bfcc384c43219c3d86e3c15dc21c52f15b7
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-08-19 16:52:57 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-l = ["apple","banana","cherry", "blueberry", "mango"]
-for i in l:
-    print(i.title())
+import random
+import string
+def generate_password(length=8):
+    c = list(string.ascii_letters + string.digits + string.punctuation)
+    password = ''.join(random.choice(c) for i in range(length))
+    return password
+print("The random password generated is:", generate_password(8))
