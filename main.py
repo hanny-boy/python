614deffa47bb55ffada6caf7d1e28a8416afa7f1
hanny-boy <hanniel.nathan.thomas@gmail.com>
2025-10-29 17:17:06 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter.filedialog import askopenfilename,asksaveasfilename
+
 window = Tk()
-window.geometry("300x300")
-window.title("Virus Detectar")
-messagebox.showwarning("window name","Text to be displayed")
-def msg():
-    messagebox.showwarning("!ALERT!","!DON'T CLICK VIRUS FOUND!")
-b = Button(window,text="SCAN FOR VIRUS",command=msg)
-b.place(x=60,y=40)
-window.mainloop()
+window.title("Text Editor")
+window.geometry("600x500")
+window.rowconfigure(0,mainsize=800,weight=1)
+window.columnconfigure(0,mainsize=800,weight=1)
+
+def open_file():
+   filepath = askopenfilename(filetypes=[("Text Files",".*.txt"),("All Files",".*.")])
+   if not filepath:
+      return
+   textedit.delete(1.0,END)
+   with open(filepath,"r") as input_file:
+      text = input_file.read()
+      textedit.insert(END,text)
+      input_file.close()
+   window.title(f"text editor"-{filepath})
+def savefile():
+   filepath = asksaveasfilename(defaultextension="txt",filetypes=[("Text Files","*.txt")])
+   if not filepath:
+      return
+   with open(filepath,"w") as output_file:
+      text = textedit.get(1.0,END)
+      output_file.write(text)
+
+   window.title(f"text editor"-{filepath})
+textedit = Text(window)
+frbuttons = Frame(window,relief=RAISED,bd = 2)
+btn_open = Button(frbuttons,text="open",command=open_file)
