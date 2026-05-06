1b37b99510eaaeb77e79b25bacab5cac3a087d6f
hanny-boy <hanniel.nathan.thomas@gmail.com>
2026-05-06 17:19:38 +1200

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,9 @@
+def Hanoi(n,a,b,c):
+    if n == 1:
+        print("move disk one from rod ",a,"to rod ",b)
+        return
+    Hanoi(n-1,a,c,b)
+    print("move disk ",n," from rod ",a," to rod",b)
+    Hanoi(n-1,c,b,a)
+n = 4
+Hanoi(n,'a','c','b')
