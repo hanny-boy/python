5535907bac4c51ecfcc3fc90d9feabcea0d345ab
hanny-boy <hanniel.nathan.thomas@gmail.com>
2026-05-13 17:12:50 +1200

    Add files via upload

---
+++ arr.py
@@ -0,0 +1,14 @@
+import array as arr
+a = arr.array('i',[])
+a.append(10)
+a.append(5)
+a.append(15)
+a.insert(3,5)
+for i in a:
+    print(i)
+b = []
+b.append(100)
+b.append(90)
+b.append(80)
+for j in b:
+    print(j)
